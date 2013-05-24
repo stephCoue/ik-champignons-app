4ae8406736e5349dbef5d033c37c6b116b667c1d
Stephane COUE <steph.coue@gmail.com>
2013-05-24 04:00:32 +0200

    init workflow [Grunt, Stylus, LiveRelead]

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    stylus: {
+      compile: {
+        options: {
+          'include css': true,
+          'paths': ['css']
+        },
+        files: {
+          'css/champignons.css': 'css/champignons.styl'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: 'css/*.styl',
+        tasks: ['stylus'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
