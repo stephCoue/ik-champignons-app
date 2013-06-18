89262bb5c075701a341beb68d4a4ee4782124141
Roland Chenel <chenel@arterrien.com>
2013-06-18 10:01:39 +0200

    Création du dossier img/fullscreen/ pour n'y stoquer que les images en haute définition (640px pour smartphone). Le dossier img/vignettes ne comprend que des images 100px 100px et sert à l'affichage en listes.

--- js/app/models/champignon.js
+++ js/app/models/champignon.js
@@ -15,7 +15,8 @@ define([
 		initialize: function() {
 			this.set({
 				"nomlatin": this.get('genre') + " " + this.get('espece').toLowerCase(),
-				"url": "#champignon"
+				"url": "#champignon",
+				"fullimage": "img/fullscreen/champignonon" + this.id + ".png"
 			});
 		}
 
