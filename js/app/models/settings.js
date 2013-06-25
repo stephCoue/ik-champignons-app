f3c6ec681a9b4c206c1d5603086d93d38a684208
Roland Chenel <chenel@arterrien.com>
2013-06-25 02:28:24 +0200

    tweek settings.js

--- js/app/models/settings.js
+++ js/app/models/settings.js
@@ -10,6 +10,19 @@ define([
 			"liststyle":"liste",
 			"sortkey":"nom",
 			"cueillette":[]
+		},
+
+		initialize: function(){
+			this.on("change", this.onChange, this);
+		},
+
+		onChange: function(event){
+			console.log("Changement !", event);
+			if(this.get("cueillette").length > 0){
+				console.log("yen a");
+			} else {
+				console.log("yen a pas");
+			}
 		}
 
 	});
